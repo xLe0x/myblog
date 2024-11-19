182d665e866c191a7c5bb5c2eb1cc3402d7afeea
xLe0x <ammarxle0x@gmail.com>
2024-11-19 22:39:35 +0200

    blah

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [tailwind(), mdx()],
+  output: 'server',
+  adapter: netlify(),
 });
